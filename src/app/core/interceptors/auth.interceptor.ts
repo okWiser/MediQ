8cba84da0a5c04a8518917632f9c3a1f72bd59ce
Wisani <wiserowens@gmail.com>
2025-08-15 13:05:15 +0200

    Add HTTP AuthInterceptor to attach JWT token from localStorage to

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../store';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private store: Store<AppState>) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Get token from store (simplified - in real app would use selector)
+    const token = localStorage.getItem('authToken');
+    
+    if (token) {
+      const authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(authReq);
+    }
+    
+    return next.handle(req);
+  }
+}
